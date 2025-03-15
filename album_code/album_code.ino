e350c3bc0e35b516207f3331b6ee39b916250258
faisalamin1303 <faisalamin1303@gmail.com>
2025-03-15 23:54:50 +0500

    Using AudioTools Library for MP3 support

--- album_code/album_code.ino
+++ album_code/album_code.ino
@@ -1,63 +1,46 @@
-#include "BluetoothA2DPSource.h"
-#include <SPI.h>
-#include <SD.h>
-#include "mp3.h"  
 
-BluetoothA2DPSource a2dp_source;
-File sound_file;
-// const char* file_name = "/audio.raw";
-const char* file_name = "/audio.mp3";
-const int sd_ss_pin = 5;
-const int frame_size_bytes = sizeof(int16_t) * 2;
+//remove this, to find issues regarding mp3 decoding
+// #define HELIX_LOGGING_ACTIVE false
+
+#include "AudioTools.h"
+#include "AudioTools/AudioLibs/A2DPStream.h"
+#include "AudioTools/Disk/AudioSourceSDFAT.h"
+#include "AudioTools/AudioCodecs/CodecMP3Helix.h"
+
+const char *startFilePath="/";
+const char* ext="mp3";
+AudioSourceSDFAT source(startFilePath, ext); // , PIN_AUDIO_KIT_SD_CARD_CS);
+A2DPStream out;
+MP3DecoderHelix decoder;
+AudioPlayer player(source, out, decoder);
 
 
-
-//mp3 var
-
-mp3 MP3;                                          // MP3 Class
-pcm audio;                                        // raw audio data
-
-// callback used by A2DP to provide the sound data
-int32_t get_sound_data(Frame* data, int32_t len) {
-  // // the data in the file must be in int16 with 2 channels 
-  // size_t result_len_bytes = sound_file.read((uint8_t*)data, len * frame_size_bytes );
-  // // result is in number of frames
-  // int32_t result_len = result_len_bytes / frame_size_bytes;
-  // ESP_LOGD("get_sound_data", "%d -> %d",len );
-  // return result_len;
-
-//----------------
-  audio = MP3.decode(); 
-                         // Decode audio data into pcm class
-  // data = (Frame*)audio.interleaved;
-  data->channel1 = audio.interleaved[0];
-  data->channel2 = audio.interleaved[1];
-  int32_t result_len = audio.nBytes / frame_size_bytes;
-  return result_len;
-}
-
-// Arduino Setup
-void setup(void) {
+void setup() {
   Serial.begin(115200);
+  AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Debug);  // Debug , Error  , Warning
 
-  // Setup SD and open file
-  SD.begin(sd_ss_pin);
-  sound_file = SD.open(file_name, FILE_READ);
- if (!sound_file) {
-    Serial.println("Failed to open /test.wav");
-    while (1);
-  }
-  Serial.println("WAV file opened successfully.");
+  // setup player
+  // Setting up SPI if necessary with the right SD pins by calling 
+  // SPI.begin(PIN_AUDIO_KIT_SD_CARD_CLK, PIN_AUDIO_KIT_SD_CARD_MISO, PIN_AUDIO_KIT_SD_CARD_MOSI, PIN_AUDIO_KIT_SD_CARD_CS);
+  player.setVolume(0.3);
+  // player.setAutoNext();
+  // player.previous();
+  // player.next();
+  player.play();
+  player.begin();
 
-  MP3.begin(sound_file);
-  audio.setSampleNumber(1);  
+  // setup output - We send the test signal via A2DP - so we conect to a Bluetooth Speaker
+  auto cfg = out.defaultConfig(TX_MODE);
+  // auto a2dp_source = out.source();
+  // a2dp_source.set_avrc_passthru_command_callback(button_handler);
+  cfg.silence_on_nodata = true; // prevent disconnect when there is no audio data
+  cfg.name = "ESP32_Player";  // set the device here. Otherwise the first available device is used for output
+  //cfg.auto_reconnect = true;  // if this is use we just quickly connect to the last device ignoring cfg.name
+  out.begin(cfg);
 
-  // start the bluetooth
-  Serial.println("starting A2DP...");
-  a2dp_source.start("ESP32_Player", get_sound_data);  
-  Serial.println("started A2DP...");
+
 }
 
-// Arduino loop - repeated processing 
 void loop() {
+  player.copy();
 }
